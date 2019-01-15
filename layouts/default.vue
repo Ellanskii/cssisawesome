6df123bb1fb867d90fccfe40c8e68c0ffc829a8b
Unknown <nameimprobable@gmail.com>
2019-01-15 22:23:03 +0300

    Scaffold
    Starting something awesome

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  nuxt/  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+    .hero.is-bold.is-info.is-fullheight
+        .container.hero-body
+            h1.title.has-text-centered CSS is awesome
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
