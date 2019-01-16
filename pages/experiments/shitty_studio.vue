ea03dbd8a031098b0bf4757eaf75be3d1bb444a0
Unknown <nameimprobable@gmail.com>
2019-01-17 00:34:07 +0300

    Уникальный контент
    Lorem ipsum dolor sit amet

--- pages/experiments/shitty_studio.vue
+++ pages/experiments/shitty_studio.vue
@@ -5,23 +5,26 @@ main
             h1.title.has-text-centered Вы получите
         .hero-body
             .container
-                ul.columns.is-multiline.is-mobile
-                    li.column
+                ul.columns.is-multiline
+                    li.column.is-6-tablet.is-4-desktop
                         h2.title.is-4 Уникальный дизайн
                         p.subtitle 
                             | Выбрать его можно 
                             <a href="">здесь</a>                  
-                    li.column
+                    li.column.is-6-tablet.is-4-desktop
                         h2.title.is-4 Продуманный UX
                         p.subtitle Отсканируйте QR-код, чтобы узнать больше
-                    li.column
+                    li.column.is-6-tablet.is-4-desktop
                         h2.title.is-4 Отличные поведенческие факторы
                         p.subtitle Время, проведённое на сайте, влияет на его позиции в поисковой выдаче
                         escaping-button
-                    li.column
+                    li.column.is-6-tablet.is-4-desktop
                         h2.title.is-4 Кратчайшие сроки
-                        p.subtitle Выйдет как выйдет, так зачем ждать?   
-                    li.column
+                        p.subtitle Выйдет как выйдет, так зачем ждать?  
+                    li.column.is-6-tablet.is-4-desktop
+                        h2.title.is-4 Уникальный контент
+                        p.subtitle Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua                        
+                    li.column.is-6-tablet.is-4-desktop
                         h2.title.is-4 Поддержка и обслуживание
                         p.subtitle 
                             | Всего за 3000&nbsp;₽ в месяц мы размесим Ваш сайт с кучей других на 
