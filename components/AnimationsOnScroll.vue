ec1e06a52c6213a6c84e429f41ee92d435f49b66
Unknown <nameimprobable@gmail.com>
2019-01-17 15:34:37 +0300

    AOS component

---
+++ components/AnimationsOnScroll.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script>
+import AOS from "aos";
+export default {
+  mounted() {
+    AOS.init();
+  }
+};
+</script>
+
+<style lang='scss'>
+@import "node_modules/aos/dist/aos";
+</style>
